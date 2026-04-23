3584e9ca24b2c7beaf8ff966944eb3be02d7628b
tommylam168 <lawliet1688@yahoo.com>
2026-04-23 17:22:27 +0800

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,36 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import './index.css'
 import App from './App.jsx'
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+//import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(outputs);
+//Amplify.configure(outputs);
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "ap-southeast-1_IMypC8CMA",
+      userPoolClientId: "2e4u9aonlr082aonpnd8vjb8cg",
+      identityPoolId: "ap-southeast-1:60b36cf0-1930-4489-a6af-390b173fa5d5",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
